079a04e06e8548caa479b0f815c697f5e8ba54e1
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-11-13 14:19:48 +0900

    project setting

---
+++ shouldbe/src/main/java/com/finalproject/team4/shouldbe/ShouldbeApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.team4.shouldbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouldbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShouldbeApplication.class, args);
+	}
+
+}
---
+++ shouldbe/src/test/java/com/finalproject/team4/shouldbe/ShouldbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.team4.shouldbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShouldbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
