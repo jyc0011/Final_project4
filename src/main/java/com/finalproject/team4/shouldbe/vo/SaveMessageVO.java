ccb8308ef1d9c12be55e7dbf6c14240c88f09872
jyc0011 <jyc0011211@gmail.com>
2023-11-30 08:44:53 +0900

    페이징 관련 시험

---
+++ src/main/java/com/finalproject/team4/shouldbe/vo/SaveMessageVO.java
@@ -0,0 +1,13 @@
+package com.finalproject.team4.shouldbe.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveMessageVO {
+    private int messageId;
+    private int userId;
+}
