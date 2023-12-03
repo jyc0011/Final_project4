fe08a02e865857acf57bfcb5e708a0b09b332ac8
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-12-03 15:40:56 +0900

    Update WebSocketConfig.java

--- src/main/java/com/finalproject/team4/shouldbe/config/WebSocketConfig.java
+++ src/main/java/com/finalproject/team4/shouldbe/config/WebSocketConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chattings").withSockJS();
 
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+	}
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "queue");
-        config.setApplicationDestinationPrefixes("/hello");
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic");
+	}
 }
